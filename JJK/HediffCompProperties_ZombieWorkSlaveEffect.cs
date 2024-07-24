7dd08d5205a13f1e5b70516b24b24b64b2200945
Bry <pheonix2105@gmail.com>
2024-07-24 04:12:44 +0100

    restructure

---
+++ JJK/HediffCompProperties_ZombieWorkSlaveEffect.cs
@@ -0,0 +1,12 @@
+﻿namespace JJK
+{
+    public class HediffCompProperties_ZombieWorkSlaveEffect : HediffCompProperties
+    {
+        public HediffCompProperties_ZombieWorkSlaveEffect()
+        {
+            compClass = typeof(HediffComp_ZombieWorkSlaveEffect);
+        }
+    }
+}
+    
+
--- Source/CursedAbilityDef.cs
+++ Source/CursedAbilityDef.cs
@@ -11,6 +11,7 @@ namespace JJK
     {
         public float cursedEnergyCost = 1f;
     }
+
 }
     
 
